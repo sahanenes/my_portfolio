15bec2606233276af58d4386ad1e3497b7e921e6
sahanenes <sahanenesss@gmail.com>
2023-04-06 01:31:18 +0300

    responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,6 +23,10 @@ export default function Home() {
                 src={profilePic}
                 alt="Enes"
                 className="w-full h-auto"
+                priority
+                sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              50vw"
               ></Image>
             </div>
             <div className="w-1/2 flex flex-col items-center self-center ">
